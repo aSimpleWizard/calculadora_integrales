f1947d279082319c35045b3dae93450fa0a94608
Jaime Hernández <59671227+aSimpleWizard@users.noreply.github.com>
2021-11-23 10:12:55 -0400

    Update 2funciones.py

--- 2funciones.py
+++ 2funciones.py
@@ -9,12 +9,12 @@ linf = int(input())
 print('Ingrese el limite superior: ')
 lsup = int(input())
 
-
+# Funcion 1
 def funcion(x):
     y = x**2 + 2*x + 3
     return y
 
-
+# Funcion 2
 def funcion2(x):
     g = x + 9
     return g
