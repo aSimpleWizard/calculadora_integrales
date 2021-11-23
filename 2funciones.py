e32268cd05bf2cad99d2a7d78379e8636f8e82cb
unknown <jaime251102@gmail.com>
2021-11-23 10:03:14 -0400

    Agregando lo que maximiliano queria

---
+++ 2funciones.py
@@ -0,0 +1,43 @@
+from numpy import arange
+from sympy import *
+from matplotlib.pyplot import *
+from numpy import *
+
+
+print('Ingrese el limite inferior: ')
+linf = int(input())
+print('Ingrese el limite superior: ')
+lsup = int(input())
+
+
+def funcion(x):
+    y = x**2 + 2*x + 3
+    return y
+
+
+def funcion2(x):
+    g = x + 9
+    return g
+
+
+# montando la grafica
+x = arange(linf, lsup, 0.1)
+y = funcion(x)
+g = funcion2(x)
+
+plot(x, y, x, g)
+fill_between(x, y, g, where=(linf <= x) & (x <= lsup), color='b', alpha=0.1)
+grid()
+
+# integrando
+x = symbols('x')
+y = funcion(x)
+g = funcion2(x)
+
+h = g-y
+
+print(h)
+res = integrate(h, (x, linf, lsup))
+print(f'la respuesta es {res}')
+
+show()
